shine5402/KiraOtoToolbox

commit a45d26f745739a4ec4dc691b49e131bfb21df83d

Add option to make ovl 1/2 when 1/3's result to small in overlap batch set

src/overlapBatchSet/overlapbatchsetotolistmodifyworker.cpp
@@ -38,7 +38,10 @@ bool OverlapBatchSetOtoListModifyWorker::doWork(const OtoEntryList& srcOtoList, 
             if (!matched.contains(i))
             {
                 auto entry = resultOtoList.at(i);
-                entry.setOverlap(entry.preUtterance() / 3);
+                auto result = entry.preUtterance() / 3;
+                if (options.getOption("oneSecondWhenTooSmall").toBool() || result < options.getOption("oneSecondWhenTooSmallValue").toDouble())
+                    result = entry.preUtterance() / 2;
+                entry.setOverlap(result);
                 resultOtoList.replace(i, entry);
             }
         }

src/overlapBatchSet/overlapbatchsetdialogoptionwidget.cpp
@@ -20,6 +20,8 @@ OverlapBatchSetDialogOptionWidget::OverlapBatchSetDialogOptionWidget(QWidget *pa
     connect(ui->oneThirdCheckBox, &QCheckBox::stateChanged, this, &ToolOptionWidget::userSettingsChanged);
     connect(ui->matchStartOtoCheckBox, &QCheckBox::stateChanged, this, &ToolOptionWidget::userSettingsChanged);
     connect(ui->setStartWithListWidget, &StringListModifyWidget::dataModified, this, &ToolOptionWidget::userSettingsChanged);
+    connect(ui->oneSecondWhenTooSmallCheckbox, &QCheckBox::stateChanged, this, &ToolOptionWidget::userSettingsChanged);
+    connect(ui->oneSecondWhenTooSmallSpinBox, qOverload<double>(&QDoubleSpinBox::valueChanged), this, &ToolOptionWidget::userSettingsChanged);
 }
 
 
@@ -37,6 +39,8 @@ OptionContainer OverlapBatchSetDialogOptionWidget::getOptions() const
     options.setOption("startWithPatternList", ui->setStartWithListWidget->getData());
     options.setOption("ifMatchStartOto", ui->matchStartOtoCheckBox->isChecked());
     options.setOption("makeOneThird", ui->oneThirdCheckBox->isChecked());
+    options.setOption("oneSecondWhenTooSmall", ui->oneSecondWhenTooSmallCheckbox->isChecked());
+    options.setOption("oneSecondWhenTooSmallValue", ui->oneSecondWhenTooSmallSpinBox->value());
     return options;
 }
 
@@ -49,6 +53,9 @@ void OverlapBatchSetDialogOptionWidget::setOptions(const OptionContainer& option
 
     ui->matchStartOtoCheckBox->setChecked(options.getOption("ifMatchStartOto").toBool());
     ui->oneThirdCheckBox->setChecked(options.getOption("makeOneThird").toBool());
+
+    ui->oneSecondWhenTooSmallCheckbox->setChecked(options.getOption("oneSecondWhenTooSmall").toBool());
+    ui->oneSecondWhenTooSmallSpinBox->setValue(options.getOption("oneSecondWhenTooSmallValue").toDouble());
 }
 
 QJsonObject OverlapBatchSetDialogOptionWidget::optionsToJson(const OptionContainer& options) const
@@ -60,6 +67,8 @@ QJsonObject OverlapBatchSetDialogOptionWidget::optionsToJson(const OptionContain
     jsonObj.insert("startWithPatternList", QJsonArray::fromStringList(options.getOption("startWithPatternList").toStringList()));
     jsonObj.insert("ifMatchStartOto", options.getOption("ifMatchStartOto").toBool());
     jsonObj.insert("makeOneThird", options.getOption("makeOneThird").toBool());
+    jsonObj.insert("oneSecondWhenTooSmall", options.getOption("oneSecondWhenTooSmall").toBool());
+    jsonObj.insert("oneSecondWhenTooSmallValue", options.getOption("oneSecondWhenTooSmallValue").toDouble());
 
     return jsonObj;
 }
@@ -73,6 +82,8 @@ OptionContainer OverlapBatchSetDialogOptionWidget::jsonToOptions(const QJsonObje
     options.setOption("startWithPatternList", getStringListFromJSONObject(json, "startWithPatternList"));
     options.setOption("ifMatchStartOto", json.value("ifMatchStartOto").toBool());
     options.setOption("makeOneThird", json.value("makeOneThird").toBool());
+    options.setOption("oneSecondWhenTooSmall", json.value("oneSecondWhenTooSmall").toBool());
+    options.setOption("oneSecondWhenTooSmallValue", json.value("oneSecondWhenTooSmallValue").toDouble());
 
     return options;
 }

src/overlapBatchSet/overlapbatchsetdialogoptionwidget.ui
@@ -7,7 +7,7 @@
     <x>0</x>
     <y>0</y>
     <width>858</width>
-    <height>411</height>
+    <height>443</height>
    </rect>
   </property>
   <layout class="QVBoxLayout" name="verticalLayout_2">
@@ -96,6 +96,67 @@
      </property>
     </widget>
    </item>
+   <item>
+    <widget class="QWidget" name="widget" native="true">
+     <property name="enabled">
+      <bool>false</bool>
+     </property>
+     <property name="minimumSize">
+      <size>
+       <width>0</width>
+       <height>0</height>
+      </size>
+     </property>
+     <layout class="QVBoxLayout" name="verticalLayout_3">
+      <property name="leftMargin">
+       <number>20</number>
+      </property>
+      <property name="topMargin">
+       <number>0</number>
+      </property>
+      <property name="rightMargin">
+       <number>0</number>
+      </property>
+      <property name="bottomMargin">
+       <number>0</number>
+      </property>
+      <item>
+       <layout class="QHBoxLayout" name="horizontalLayout_4">
+        <item>
+         <widget class="QCheckBox" name="oneSecondWhenTooSmallCheckbox">
+          <property name="text">
+           <string>结果小于该值时进行二分之一化</string>
+          </property>
+         </widget>
+        </item>
+        <item>
+         <spacer name="horizontalSpacer_2">
+          <property name="orientation">
+           <enum>Qt::Horizontal</enum>
+          </property>
+          <property name="sizeHint" stdset="0">
+           <size>
+            <width>40</width>
+            <height>20</height>
+           </size>
+          </property>
+         </spacer>
+        </item>
+        <item>
+         <widget class="QDoubleSpinBox" name="oneSecondWhenTooSmallSpinBox">
+          <property name="decimals">
+           <number>3</number>
+          </property>
+          <property name="maximum">
+           <double>99999.000000000000000</double>
+          </property>
+         </widget>
+        </item>
+       </layout>
+      </item>
+     </layout>
+    </widget>
+   </item>
   </layout>
  </widget>
  <customwidgets>
@@ -137,6 +198,22 @@
     <hint type="destinationlabel">
      <x>766</x>
      <y>15</y>
+    </hint>
+   </hints>
+  </connection>
+  <connection>
+   <sender>oneThirdCheckBox</sender>
+   <signal>toggled(bool)</signal>
+   <receiver>widget</receiver>
+   <slot>setEnabled(bool)</slot>
+   <hints>
+    <hint type="sourcelabel">
+     <x>147</x>
+     <y>387</y>
+    </hint>
+    <hint type="destinationlabel">
+     <x>163</x>
+     <y>414</y>
     </hint>
    </hints>
   </connection>

commit 3cdbf889d0b802db873c373a48cf8914efb879b4

fix issues with #a45d26f, which caused by incorrect bool operation

src/overlapBatchSet/overlapbatchsetotolistmodifyworker.cpp
@@ -39,7 +39,7 @@ bool OverlapBatchSetOtoListModifyWorker::doWork(const OtoEntryList& srcOtoList, 
             {
                 auto entry = resultOtoList.at(i);
                 auto result = entry.preUtterance() / 3;
-                if (options.getOption("oneSecondWhenTooSmall").toBool() || result < options.getOption("oneSecondWhenTooSmallValue").toDouble())
+                if (options.getOption("oneSecondWhenTooSmall").toBool() && result < options.getOption("oneSecondWhenTooSmallValue").toDouble())
                     result = entry.preUtterance() / 2;
                 entry.setOverlap(result);
                 resultOtoList.replace(i, entry);
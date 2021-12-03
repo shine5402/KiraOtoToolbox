9ee24b65ba15dd08ba2691e470923d9bf8a26f6e
shine_5402 <shine_5402@live.cn>
2021-12-03 22:57:15 +0800

    Revert "Add error messgebox when processing failed"
    This reverts commit e4e16cf24fc783a9a9fb9042d38baa5a64c82ca4.

--- src/toolBase/tooldialog.cpp
+++ src/toolBase/tooldialog.cpp
@@ -106,8 +106,6 @@ void ToolDialog::accept()
         QMessageBox::information(this, {}, tr("Operation completed successfully."));
         QDialog::accept();
     }
-    else
-        QMessageBox::critical(this, {}, tr("Error occured while processing. Please check and try again."));
 }
 
 void ToolDialog::reset()

6b6b5ceefa4595e98046c4c627253bc17476edd5
shine_5402 <shine_5402@live.cn>
2022-02-07 15:49:02 +0800

    Now user can select if field need to be changed in PreCenteredChaneValue tool

--- src/preCenteredChangeValue/precenteredchangevalueoptionwidget.cpp
+++ src/preCenteredChangeValue/precenteredchangevalueoptionwidget.cpp
@@ -8,6 +8,10 @@ PreCenteredChangeValueOptionWidget::PreCenteredChangeValueOptionWidget(QWidget *
     ui->setupUi(this);
     connect(ui->normalizeWithTempoButton, &QPushButton::clicked, this, &PreCenteredChangeValueOptionWidget::setParamsWithUITempo);
 
+    connect(ui->constFieldCheckBox, &QCheckBox::toggled, this, &ToolOptionWidget::userSettingsChanged);
+    connect(ui->preutteranceCheckBox, &QCheckBox::toggled, this, &ToolOptionWidget::userSettingsChanged);
+    connect(ui->overlapCheckBox, &QCheckBox::toggled, this, &ToolOptionWidget::userSettingsChanged);
+    connect(ui->rightCheckBox, &QCheckBox::toggled, this, &ToolOptionWidget::userSettingsChanged);
     connect(ui->constFieldDoubleSpinBox, qOverload<double>(&QDoubleSpinBox::valueChanged), this, &ToolOptionWidget::userSettingsChanged);
     connect(ui->preutteranceDoubleSpinBox, qOverload<double>(&QDoubleSpinBox::valueChanged), this, &ToolOptionWidget::userSettingsChanged);
     connect(ui->overlapDoubleSpinBox, qOverload<double>(&QDoubleSpinBox::valueChanged), this, &ToolOptionWidget::userSettingsChanged);
@@ -33,9 +37,13 @@ OptionContainer PreCenteredChangeValueOptionWidget::getOptions() const
 {
     OptionContainer options;
 
+    options.setOption("changeConstField", ui->constFieldCheckBox->isChecked());
     options.setOption("constField", ui->constFieldDoubleSpinBox->value());
+    options.setOption("changePreutterance", ui->preutteranceCheckBox->isChecked());
     options.setOption("preutterance", ui->preutteranceDoubleSpinBox->value());
+    options.setOption("changeOverlap", ui->overlapCheckBox->isChecked());
     options.setOption("overlap", ui->overlapDoubleSpinBox->value());
+    options.setOption("changeRight", ui->rightCheckBox->isChecked());
     options.setOption("right", ui->rightDoubleSpinBox->value());
 
     return options;
@@ -43,9 +51,13 @@ OptionContainer PreCenteredChangeValueOptionWidget::getOptions() const
 
 void PreCenteredChangeValueOptionWidget::setOptions(const OptionContainer& options)
 {
+    ui->constFieldCheckBox->setChecked(options.getOption("changeConstField", true).toBool());
     ui->constFieldDoubleSpinBox->setValue(options.getOption("constField", 375.0).toDouble());//3/4
+    ui->preutteranceCheckBox->setChecked(options.getOption("changePreutterance", true).toBool());
     ui->preutteranceDoubleSpinBox->setValue(options.getOption("preutterance", 250.0).toDouble());//1/2
+    ui->overlapCheckBox->setChecked(options.getOption("changeOverlap", true).toBool());
     ui->overlapDoubleSpinBox->setValue(options.getOption("overlap", 83.333).toDouble());//1/6
+    ui->rightCheckBox->setChecked(options.getOption("changeRight", true).toBool());
     ui->rightDoubleSpinBox->setValue(options.getOption("right", -583.333).toDouble());//all + overlap
 }
 
@@ -53,9 +65,13 @@ QJsonObject PreCenteredChangeValueOptionWidget::optionsToJson(const OptionContai
 {
     QJsonObject json;
 
+    json.insert("changeConstField", options.getOption("changeConstField").toBool());
     json.insert("constField", options.getOption("constField").toDouble());
+    json.insert("changePreutterance", options.getOption("changePreutterance").toBool());
     json.insert("preutterance", options.getOption("preutterance").toDouble());
+    json.insert("changeOverlap", options.getOption("changeOverlap").toBool());
     json.insert("overlap", options.getOption("overlap").toDouble());
+    json.insert("changeRight", options.getOption("changeRight").toBool());
     json.insert("right", options.getOption("right").toDouble());
 
     return json;
@@ -65,9 +81,13 @@ OptionContainer PreCenteredChangeValueOptionWidget::jsonToOptions(const QJsonObj
 {
     OptionContainer options;
 
+    options.setOption("changeConstField", json.value("changeConstField").toBool(true));
     options.setOption("constField", json.value("constField").toDouble(375.0));
+    options.setOption("changePreutterance", json.value("changePreutterance").toBool(true));
     options.setOption("preutterance", json.value("preutterance").toDouble(250.0));
+    options.setOption("changeOverlap", json.value("changeOverlap").toBool(true));
     options.setOption("overlap", json.value("overlap").toDouble(83.333));
+    options.setOption("changeRight", json.value("changeRight").toBool(true));
     options.setOption("right", json.value("right").toDouble(-583.333));
 
     return options;
--- src/preCenteredChangeValue/precenteredchangevalueotolistmodifyworker.cpp
+++ src/preCenteredChangeValue/precenteredchangevalueotolistmodifyworker.cpp
@@ -11,19 +11,31 @@ bool PreCenteredChangeValueOtoListModifyWorker::doWork(const OtoEntryList& srcOt
 {
     Q_UNUSED(secondSaveOtoList)
 
+    auto changeConstField = options.getOption("changeConstField").toBool();
     auto constField = options.getOption("constField").toDouble();
+    auto changePreutterance = options.getOption("changePreutterance").toBool();
     auto preutterance = options.getOption("preutterance").toDouble();
+    auto changeOverlap = options.getOption("changeOverlap").toBool();
     auto overlap = options.getOption("overlap").toDouble();
+    auto changeRight = options.getOption("changeRight").toBool();
     auto right = options.getOption("right").toDouble();
     resultOtoList = {};
     for (const auto& entry : srcOtoList){
         auto newEntry = entry;
-        auto newLeft = entry.preUtterance() - preutterance + entry.left();
-        newEntry.setLeft(newLeft >= 0 ? newLeft : entry.left());
-        newEntry.setPreUtterance(newLeft >= 0 ? preutterance : entry.preUtterance());
-        newEntry.setConsonant(constField);
-        newEntry.setOverlap(overlap);
-        newEntry.setRight(right);
+        if (changePreutterance){
+            auto preDiff = entry.preUtterance() - preutterance;
+            auto newLeft = preDiff + entry.left();
+            newEntry.setLeft(newLeft >= 0 ? newLeft : entry.left());
+            newEntry.setPreUtterance(newLeft >= 0 ? preutterance : entry.preUtterance());
+            //To ensure new const field not smaller than pre
+            newEntry.setConsonant(entry.consonant() - preDiff);
+        }
+        if (changeConstField)
+            newEntry.setConsonant(constField);
+        if (changeOverlap)
+            newEntry.setOverlap(overlap);
+        if (changeRight)
+            newEntry.setRight(right);
         resultOtoList.append(newEntry);
     }
 
